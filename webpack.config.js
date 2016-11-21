484461d2d895bbba45235f64275e9527350ff824
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2016-11-21 13:01:20 +0200

    fix css-loader less-loader

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
     module:{
         loaders: [
             { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
-            { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader", "less-loader") }
+            { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") }
         ]
     },
     plugins: [
